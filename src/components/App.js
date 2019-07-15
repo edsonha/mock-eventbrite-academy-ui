c4fede7807c6a4eb2f0a84441f290f1d338ab5ee
brandonngiam <brandonngiam@user.noreply.github.com>
2019-07-15 11:48:48 +0800

    [Test #4][B&D][Trigger redeploy to Circleci]

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "../styles/App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return <div className="App">Stashaway</div>;
 }
 
 export default App;
