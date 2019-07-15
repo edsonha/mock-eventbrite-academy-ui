7555a0deddd80f89d742c68acbccf39b22155541
edsonha <edsonha@gmail.com>
2019-07-15 16:17:08 +0800

    [8][Browsing][JJ&Eddie] Build Responsive Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,22 @@
 import React from "react";
 import { ReactComponent as Logo } from "../assets/logo.svg";
-import { Navbar, NavbarBrand, Nav, NavItem, Button } from "reactstrap";
+import { Navbar, NavbarBrand, Button } from "reactstrap";
 import "../styles/Header.css";
 
 const Header = () => {
   return (
-    <Navbar className="navbar" light expand="md">
+    <Navbar className="navbar col" light expand="sm">
       <NavbarBrand href="/">
         <Logo data-testid="logo-svg" />
       </NavbarBrand>
-      <Nav className="ml-auto" navbar>
-        <NavItem>
-          <Button className="login-button" href="/">
-            Log In
-          </Button>
-        </NavItem>
-        <NavItem>
-          <Button className="signup-button" href="/">
-            Sign Up
-          </Button>
-        </NavItem>
-      </Nav>
+      <div className="button-wrapper">
+        <Button className="login-button" href="/">
+          Log In
+        </Button>
+        <Button className="signup-button" href="/">
+          Sign Up
+        </Button>
+      </div>
     </Navbar>
   );
 };
