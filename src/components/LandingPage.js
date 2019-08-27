e3d1e7a9e071bd5583e2df8d45cd187bab7a5bef
edsonha <edsonha@gmail.com>
2019-08-27 18:13:42 +0800

    Change config.yml to push to Netlify

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -42,7 +42,7 @@ class LandingPage extends React.Component {
     return (
       <div>
         <div className="title-banner">
-          <h1>Personal Finance Courses on Your Terms</h1>
+          <h1>Finance Courses on Your Terms</h1>
         </div>
 
         <Container className="stashaway-academy-description">
